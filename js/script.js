f55e5dab44b54cf1bde542f549642e44645a7383
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-28 15:12:48 +0100

    feat: avanzamento milestone 1

---
+++ js/script.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            tasks: [
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    text: "Andare al mare",
+                    done: false
+                },
+                {
+                    text: "Aiutare papà",
+                    done: false
+                },
+                {
+                    text: "Studiare vue.js",
+                    done: false
+                }
+            ],
+            currentIndex: null,
+
+        }
+
+    },
+    methods: {
+        checkIfDone(index) {
+            this.currentIndex = index;
+            if (this.tasks[this.currentIndex].done === true) {
+                this.tasks[this.currentIndex].done = false
+                return false
+            }
+            else {
+                this.tasks[this.currentIndex].done = true;
+                return true
+            }
+        }
+    },
+    mounted() {
+    }
+}).mount('#app')
+
+
+
